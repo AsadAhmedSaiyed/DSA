f2333156b700f1ffa2aa4ea6550d21f7b10cbf54
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2025-10-28 17:32:38 +0530

    Uploaded questions for stacks

---
+++ Stacks/PushAtBottom.java
@@ -0,0 +1,25 @@
+
+import java.util.Stack;
+
+public class PushAtBottom {
+    public static  void pushAtBottom(Stack<Integer> st,int x){
+        if(st.isEmpty() == true){
+            st.push(x);
+            return;
+        }
+       int data = st.pop();
+       pushAtBottom(st, x);
+       st.push(data);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        pushAtBottom(s,4);
+        while(!s.isEmpty()){
+         System.out.println(s.peek());
+         s.pop();
+      }
+    }
+}
