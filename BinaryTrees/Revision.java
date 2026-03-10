ce4f0427b906352a72a7e5716e5c40535ffbac93
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-03-10 21:22:42 +0530

    leetcode 124

--- BinaryTrees/Revision.java
+++ BinaryTrees/Revision.java
@@ -1,20 +1,22 @@
+public class Revision {
+    static class Node {
+        int val;
+        Node left;
+        Node right;
 
-public class Revision{
-    static class Node{
-       int val;
-       Node left;
-       Node right;
-       Node(int data){
-          this.val = data;
-          this.left = null;
-          this.right = null;
-       }
+        Node(int data) {
+            this.val = data;
+            this.left = null;
+            this.right = null;
+        }
     }
-    static class BTree{
+
+    static class BTree {
         static int idx = -1;
-        public static Node build(int nodes[]){
+
+        public static Node build(int nodes[]) {
             idx++;
-            if(nodes[idx] == -1){
+            if (nodes[idx] == -1) {
                 return null;
             }
             Node newNode = new Node(nodes[idx]);
@@ -22,25 +24,46 @@ public class Revision{
             newNode.right = build(nodes);
             return newNode;
         }
-       public boolean isUnivalTree(Node root) {
-        if(root == null){
-            return true;
-        }
-        if(root.left != null && root.val != root.left.val){
+
+        public boolean isUnivalTree(Node root) {
+            if (root == null) {
+                return true;
+            }
+            if (root.left != null && root.val != root.left.val) {
+                return false;
+            }
+            if (root.right != null && root.val != root.right.val) {
+                return false;
+            }
+            boolean foundLeft = isUnivalTree(root.left);
+            boolean foundRight = isUnivalTree(root.right);
+            if (foundLeft && foundRight) {
+                return true;
+            }
             return false;
         }
-        if(root.right != null && root.val != root.right.val){
-            return false;
+
+        int sum = Integer.MIN_VALUE;
+
+        private int helper(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int leftSum = Math.max(helper(root.left), 0);
+            int rightSum = Math.max(helper(root.right), 0);
+            int pathSum = root.val + leftSum + rightSum;
+            sum = Math.max(sum, pathSum);
+
+            return root.val + Math.max(leftSum, rightSum);
         }
-        boolean foundLeft = isUnivalTree(root.left);
-        boolean foundRight = isUnivalTree(root.right);
-        if(foundLeft && foundRight){
-            return true;
+
+        public int maxPathSum(TreeNode root) {
+            helper(root);
+            return sum;
         }
-        return false;
     }
+
+    public static void main(String[] args) {
+
     }
-   public static void main(String[] args) {
-       
-   }   
 }
