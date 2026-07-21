3589e6fb931faf204b8acad44f5236f051a7193e
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-07-21 21:15:37 +0530

    leetcode 312 Memo

--- DP/BurstBallons.java
+++ DP/BurstBallons.java
@@ -23,4 +23,31 @@ public class BurstBallons{
         }
         return helper(arr);
     }
+    private int helper(int arr[], int l, int r, int dp[][] ){
+       if(l > r){
+         return 0;
+       }
+       if(dp[l][r] != 0){
+         return dp[l][r];
+       }
+       
+       for(int i=l+1;i<r;i++){
+          int coins = arr[l]*arr[i]*arr[r];
+          coins += (helper(arr,l,i,dp) + helper(arr,i,r,dp));
+          dp[l][r] = Math.max(dp[l][r], coins);
+       }
+       return dp[l][r];
+    }
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int arr[] = new int[n+2];
+        arr[0] = arr[n+1] = 1;
+        for(int i=0;i<n;i++){
+            arr[i+1] = nums[i];
+        }
+        int[][] dp = new int[n + 2][n + 2];
+        return helper(arr,0,n+1,dp);
+        
+    }
 }
