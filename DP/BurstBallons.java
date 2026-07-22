b59aed45e162a48d459c5613246ef36d65ac2ace
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-07-22 21:08:48 +0530

    leetcode 312 tabulation

--- DP/BurstBallons.java
+++ DP/BurstBallons.java
@@ -23,7 +23,7 @@ public class BurstBallons{
         }
         return helper(arr);
     }
-    private int helper(int arr[], int l, int r, int dp[][] ){
+    private int helper2(int arr[], int l, int r, int dp[][] ){
        if(l > r){
          return 0;
        }
@@ -33,13 +33,13 @@ public class BurstBallons{
        
        for(int i=l+1;i<r;i++){
           int coins = arr[l]*arr[i]*arr[r];
-          coins += (helper(arr,l,i,dp) + helper(arr,i,r,dp));
+          coins += (helper2(arr,l,i,dp) + helper2(arr,i,r,dp));
           dp[l][r] = Math.max(dp[l][r], coins);
        }
        return dp[l][r];
     }
 
-    public int maxCoins(int[] nums) {
+    public int maxCoins2(int[] nums) {
         int n = nums.length;
         int arr[] = new int[n+2];
         arr[0] = arr[n+1] = 1;
@@ -47,7 +47,29 @@ public class BurstBallons{
             arr[i+1] = nums[i];
         }
         int[][] dp = new int[n + 2][n + 2];
-        return helper(arr,0,n+1,dp);
+        return helper2(arr,0,n+1,dp);
         
+    }
+    public int maxCoins3(int[] nums) {
+        int n = nums.length;
+        int arr[] = new int[n+2];
+        arr[0] = arr[n+1] = 1;
+        for(int i=0;i<n;i++){
+            arr[i+1] = nums[i];
+        }
+        int[][] dp = new int[n + 2][n + 2];
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+
+                int max = Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++){
+                    int cost = arr[i-1]*arr[k]*arr[j+1] + dp[i][k-1] + dp[k+1][j];
+                    max = Math.max(max,cost);
+                }
+                dp[i][j] = max;
+            }
+        }
+        return dp[1][n];
     }
 }
