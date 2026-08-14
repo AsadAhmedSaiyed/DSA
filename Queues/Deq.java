73b28866444ddb6d9f5192de48a2f5f4f5cba03a
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-08-14 22:17:33 +0530

    leetcode 3

--- Queues/Deq.java
+++ Queues/Deq.java
@@ -4,6 +4,7 @@ public class Deq {
     public static void main(String[] args) {
         Deque<Integer> d = new LinkedList<>();
         d.addFirst(1);
+    
         d.addFirst(2);
         d.addLast(3);
         System.out.println(d);
---
+++ Strings/LongestSubstrWithoutRC.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class LongestSubstrWithoutRC{
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int max = 0;
+        int l = 0;
+        for(int i=0;i<s.length();i++){
+           while(set.contains(s.charAt(i))){
+             set.remove(s.charAt(l));
+             l++;
+           }
+           set.add(s.charAt(i));
+           max = Math.max(max, i-l+1);
+        }
+        return max;
+    }
+}
