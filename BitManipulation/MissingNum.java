9191179dae6562f946be58a463730eb7f5e96b67
AsadAhmedSaiyed <166991578+AsadAhmedSaiyed@users.noreply.github.com>
2026-09-10 10:33:58 +0530

    more on bit manipulation

---
+++ BitManipulation/MissingNum.java
@@ -0,0 +1,11 @@
+public class MissingNum{
+     public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ans = n;
+        for(int i=0;i<n;i++){
+           ans = ans ^ i;
+           ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
